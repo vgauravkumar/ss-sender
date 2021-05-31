c828b373149c26abfc5132617d288f02bf681a80
Gaurav <gauravkumarverma2001@gmail.com>
2021-05-31 23:00:16 +0530

    Created main.py and an empty 'ss' folder.

---
+++ main.py
@@ -0,0 +1,44 @@
+import discord
+import pyautogui
+import os
+
+#variables
+location = r'C:\Users\KIIT\PycharmProjects\ssSender\ss\ss.png'
+TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+#Code starts
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    # CHECK IF BOT IS THE SENDER
+    if message.author == client.user:
+        return
+
+    # WELCOME
+    if message.content.startswith('$hello'):  # Verified
+        try:
+            await message.channel.send('Hi ' + str('<@' + str(message.author.id) + '>') + ' !')
+        except:
+            await message.channel.send('Hi ' + message.author.name + ' !\nUnknown error occured.')
+
+    if message.content.startswith('$ss'):
+        try :
+            myScreenshot = pyautogui.screenshot()
+            myScreenshot.save(r'C:\Users\KIIT\PycharmProjects\ssSender\ss\ss.png')
+            await message.channel.send(file=discord.File(r'C:\Users\KIIT\PycharmProjects\ssSender\ss\ss.png'))
+            try:
+                os.remove(r'C:\Users\KIIT\PycharmProjects\ssSender\ss\ss.png')
+            except:
+                pass
+        except :
+            await message.channel.send('Hi ' + message.author.name + ' !\nUnknown error occured.')
+
+
+# keep_alive()
+client.run(TOKEN)
